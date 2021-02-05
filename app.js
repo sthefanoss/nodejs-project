d9519fa70c6e1f8a04f0c300239cf4593c0e4b73
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-04 23:50:12 -0300

    add "/message" route

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+    if(request.url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html>');
+        response.write('    <head><title>Enter message</title></head>');
+        response.write('    <body><form action="/message" method="POST">');
+        response.write('        <input type="text" name="message">');
+        response.write('        <button type="submit">Send</button>');
+        response.write('    </from></body>');
+        response.write('</html>');
+        response.end();
+        return 
+    }
+    
     console.log(request);
-
     response.setHeader('Content-Type', 'text/html');
     response.write('<html>');
-    response.write('<head><title>Hello world!</title></head>');
-    response.write('<body><h1>Hello from my first Node.js server.</h1></body>');
+    response.write('    <head><title>Hello world!</title></head>');
+    response.write('    <body><h1>Hello from my first Node.js server.</h1></body>');
     response.write('</html>');
 
     response.end();
