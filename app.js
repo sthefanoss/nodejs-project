61f4c3ac9c26fc83ab2a60883539798242af545b
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-06 20:19:41 -0300

    make "/add-product" route

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const server = express();
 
-server.use((request, response, next) => {
-    console.log('In the middleware!', request.url);
+server.use('/', (request, response, next) => {
+    console.log('This always runs!');
     next();
 });
 
-server.use((request, response, next) => {
-    console.log('In another middleware', request.url);
+server.use('/add-product', (request, response, next) => {
+    response.send('<h1>The add product page</h1>');
+});
+
+server.use('/', (request, response, next) => {
     response.send('<h1>Hello from express</h1>');
 });
 
