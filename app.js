54161864fb81c3fff896386a65e8c9d4b6849109
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-06 20:13:19 -0300

    start new routes and use a middleware

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const router = require('./router');
 
 const server = express();
 
-server.use(router);
+server.use((request, response, next) => {
+    console.log('In the middleware!', request.url);
+    next();
+});
+
+server.use((request, response, next) => {
+    console.log('In another middleware', request.url);
+    response.send('<h1>Hello from express</h1>');
+});
 
 server.listen(9000);
