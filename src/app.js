694f5217de9e8f0cd347464067afcc3f92f0641d
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-06 22:28:37 -0300

    serve css statically

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('./util/path');
+
 const authRouter = require('./routes/auth');
 const shopRouter = require('./routes/shop');
 const errorRouter = require('./routes/error');
@@ -8,6 +10,8 @@ const errorRouter = require('./routes/error');
 const server = express();
 
 server.use(bodyParser.urlencoded({extended: false}));
+server.use(express.static(path('public')));
+
 server.use(authRouter);
 server.use(shopRouter);
 server.use(errorRouter);
