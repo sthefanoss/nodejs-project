5bc5564af7ac166d2693261bcfbfcb438ecdfe8f
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-21 18:27:37 -0300

    add database initialize script

--- src/models/cart.js
+++ src/models/cart.js
@@ -2,7 +2,7 @@ const dataBase = require('../util/database');
 
 module.exports = class Cart{
     static get() {
-        return dataBase.execute(
+        return dataBase.pool.execute(
             'SELECT * FROM cart_items',
         ).then(([entries, rows]) => {
            return new Map(entries.map(entry =>[Number(entry.product_id), Number(entry.quantity)]));
@@ -13,13 +13,13 @@ module.exports = class Cart{
         id = Number(id);
         return Cart.get().then(entries => {
             if(entries.has(id)) {
-                return dataBase.execute(
+                return dataBase.pool.execute(
                     'UPDATE cart_items SET quantity=? WHERE product_id=?',
                     [entries.get(id)+1, id]
                 );
             }
 
-            return dataBase.execute(
+            return dataBase.pool.execute(
                 'INSERT INTO cart_items (product_id, quantity) VALUES (?, ?)',
                 [id, 1],
             );
@@ -34,13 +34,13 @@ module.exports = class Cart{
             }
             
             if(entries.get(id) > 1) {
-            return dataBase.execute(
+            return dataBase.pool.execute(
                     'UPDATE cart_items SET quantity=? WHERE product_id=?',
                     [entries.get(id) - 1, id]
                 );
             }
 
-            return dataBase.execute(
+            return dataBase.pool.execute(
                 'DELETE FROM cart_items where product_id=?',
                 [id],
             );
--- src/util/database.js
+++ src/util/database.js
@@ -3,9 +3,30 @@ const mySql = require('mysql2');
 const pool = mySql.createPool({
     host: 'localhost',
     port: 3306,
-    database: 'node-js',
+    database: 'node_js',
     user:'root',
     password:'',
 });
 
-module.exports = pool.promise();
+module.exports.initialize = () => {
+    return pool.promise()
+    .execute(String.raw`
+    CREATE TABLE IF NOT EXISTS node_js.cart_items (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id INT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id));`)
+    .then(() => pool.promise()
+    .execute(String.raw`
+    CREATE TABLE IF NOT EXISTS node_js.products (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    price DOUBLE UNSIGNED NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);`)
+    )
+};
+
+module.exports.pool = pool.promise();
