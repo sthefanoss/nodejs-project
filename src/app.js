498fc2450da193b065d10d248bb8653d29c8d77a
Sthefano Schiavon <sthefanoss@gmail.com>
2021-02-23 22:30:50 -0300

    make user and product relations and add user to request in middleware

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 
 const path = require('./util/path');
 const dataBase = require('./util/database');
-
 const authRouter = require('./routes/auth');
 const shopRouter = require('./routes/shop');
 const errorRouter = require('./routes/error');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const server = express();
 server.set('view engine', 'pug');
@@ -15,10 +16,25 @@ server.set('views', './src/views');
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(express.static(path('public')));
 
+server.use((request, response, next) =>
+    User.findByPk(1)
+    .then(user => request.user = user)
+    .catch(error => console.log(error))
+    .finally(() => next())
+);
 server.use('/auth', authRouter);
 server.use(shopRouter);
 server.use(errorRouter);
 
-dataBase.sync().then(() => server.listen(9000))
-               .catch(e => console.log(e));
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
 
+dataBase.sync()
+.then(() => User.findByPk(1))
+.then(user => {
+    if(user == null) 
+        return User.create({name: 'Sthefano', email:'tefs@gmail.com'});
+    return Promise.resolve(user);
+})
+.then(() => server.listen(9000))
+.catch(e => console.log(e));
